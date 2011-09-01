e3ef1cc0a23785ee527723c2035c2a909fb2f36d
Seth Stone <seth.stone@gmail.com>
2011-09-01 20:44:51 +0000

    using herokus nodejs hello world example, instead of original one

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
-// Load the http module to create an http server.
-var http = require('http');
+var express = require('express');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(80);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
