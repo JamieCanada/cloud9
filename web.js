7f9f310ebd9e0f1d165108c8cbfeaec50e602ae8
Seth Stone <seth.stone@gmail.com>
2011-09-12 18:00:43 -0500

    Changed to hello world 3

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!' + process.env.PORT);
+  response.send('Hello World3!' + process.env.PORT);
 });
 
 var port = process.env.PORT || 3000;
